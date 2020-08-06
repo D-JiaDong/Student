4f59405eedb567b1f25e9e949a9acff861dc5aa3
71F54A01 <1911900673@qq.com>
2020-08-06 22:48:19 +0800

    优化

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int dir[4][2]={1,0,0,1,-1,0,0,-1};
 bool bool_[120][120];
 int n,m;
 void dfs(int x,int y){
-    if(x<0||x>=n||y<0||y>=m||map[x][y]=='0'||bool_[x][y]||map[x][y] == '*'){
+    if(x<0||x>=n||y<0||y>=m||map[x][y]=='0'||bool_[x][y]){
         return;
     }
     bool_[x][y]= true;
@@ -15,7 +15,8 @@ void dfs(int x,int y){
         int X=x+dir[i][0];
         int Y=y+dir[i][1];
         if(X>=0&&Y>=0&&X<n&&Y<m) {
-            if (map[X][Y] == '*' || bool_[X][Y])
+            //四周存在标记过的节点
+            if (bool_[X][Y])
                 k++;
         }
     }
